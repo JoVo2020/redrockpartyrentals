63d9985aa8ca8c1e4c634d6d8db2a6fba51f607f
JoVo2020 <joel.m.voss@gmail.com>
2026-03-01 17:39:03 -0800

    product page\

---
+++ public/product_data.js
@@ -0,0 +1,175 @@
+
+window.PRODUCT_DATA = [
+
+	{
+      "Item_ID": "Table",
+      "Product_ID": "7069036000000622084",
+      "Item Name": "Table",
+      "Item Description": "Regular, foldable, 6ft white table, seats 6-8 people (3 on each side, 1 one on each end)",
+      "Quantity Available": 12,
+      "Rental Price": 8,
+      "Category": "Tables And Chairs",
+      "Image": "https://redrockpartyrentals.com/images/table.jpg"
+    },
+    {
+      "Item_ID": "Chair",
+      "Product_ID": "7069036000000633191",
+      "Item Name": "Chair",
+      "Item Description": "Regular, foldable, white chair",
+      "Quantity Available": 120,
+      "Rental Price": 1,
+      "Category": "Tables And Chairs",
+      "Image": "https://redrockpartyrentals.com/images/chair3.jpg"
+    },
+    {
+      "Item_ID": "Cocktail_Table",
+      "Product_ID": "7069036000001145028",
+      "Item Name": "Cocktail Table",
+      "Item Description": "Black bar height cocktail table",
+      "Quantity Available": 2,
+      "Rental Price": 13,
+      "Category": "Tables And Chairs",
+      "Image": "https://redrockpartyrentals.com/images/cocktail-table.jpg"
+    },
+    {
+      "Item_ID": "White_Table_Cover",
+      "Product_ID": "7069036000001145033",
+      "Item Name": "White Table Cover",
+      "Item Description": "White, regular table cover (unfitted)",
+      "Quantity Available": 4,
+      "Rental Price": 5,
+      "Category": "Table Covers",
+      "Image": "https://redrockpartyrentals.com/images/tablecloth-regular.jpg"
+    },
+    {
+      "Item_ID": "Fitted_Table_Cover",
+      "Product_ID": "7069036000001145038",
+      "Item Name": "Fitted Table Cover",
+      "Item Description": "White, fitted table cover",
+      "Quantity Available": 4,
+      "Rental Price": 5,
+      "Category": "Table Covers",
+      "Image": "https://redrockpartyrentals.com/images/tablecloth-fitted.jpg"
+    },
+    {
+      "Item_ID": "Fitted_Cocktail_Table_Cover_White",
+      "Product_ID": "7069036000001145043",
+      "Item Name": "Fitted Cocktail Table Cover (White)",
+      "Item Description": "White, fitted, cocktail table cover",
+      "Quantity Available": 2,
+      "Rental Price": 5,
+      "Category": "Table Covers",
+      "Image": "https://redrockpartyrentals.com/images/cocktail-tablecover-white.jpg"
+    },
+    {
+      "Item_ID": "Fitted_Cocktail_Table_Cover_Black",
+      "Product_ID": "7069036000001467015",
+      "Item Name": "Fitted Cocktail Table Cover (Black)",
+      "Item Description": "Black, fitted, cocktail table cover",
+      "Quantity Available": 2,
+      "Rental Price": 5,
+      "Category": "Table Covers",
+      "Image": "https://redrockpartyrentals.com/images/cocktail-tablecover-black.jpg"
+    },
+    {
+      "Item_ID": "Blue_Table_Runner",
+      "Product_ID": "7069036000001145048",
+      "Item Name": "Blue Table Runner",
+      "Item Description": "Light Blue table runner",
+      "Quantity Available": 4,
+      "Rental Price": 2,
+      "Category": "Table Runners",
+      "Image": "https://redrockpartyrentals.com/images/tablerunner-dustyblue.jpg"
+    },
+    {
+      "Item_ID": "Green_Table_Runner",
+      "Product_ID": "7069036000001145053",
+      "Item Name": "Green Table Runner",
+      "Item Description": "Sage Green table runner",
+      "Quantity Available": 4,
+      "Rental Price": 2,
+      "Category": "Table Runners",
+      "Image": "https://redrockpartyrentals.com/images/tablerunner-sagegreen.jpg"
+    },
+    {
+      "Item_ID": "Standing_Cooler",
+      "Product_ID": "7069036000001145058",
+      "Item Name": "Standing Cooler",
+      "Item Description": "Standing, Coca-cola branded cooler",
+      "Quantity Available": 1,
+      "Rental Price": 30,
+      "Category": "Other",
+      "Image": "https://redrockpartyrentals.com/images/StandingCooler.jpg"
+    },
+    {
+      "Item_ID": "Giant_Connect_4_Game",
+      "Product_ID": "7069036000000633662",
+      "Item Name": "Giant Connect 4 Game",
+      "Item Description": "Giant Connect 4 game",
+      "Quantity Available": 2,
+      "Rental Price": 25,
+      "Category": "Games",
+      "Image": "https://redrockpartyrentals.com/images/GiantConnect4.jpg"
+    },
+    {
+      "Item_ID": "Corn_Hole_Game",
+      "Product_ID": "7069036000001145063",
+      "Item Name": "Corn Hole Game",
+      "Item Description": "Corn Hole game",
+      "Quantity Available": 1,
+      "Rental Price": 25,
+      "Category": "Games",
+      "Image": "https://redrockpartyrentals.com/images/Cornhole.jpg"
+    },
+    {
+      "Item_ID": "Giant_Jenga_Game",
+      "Product_ID": "7069036000001145068",
+      "Item Name": "Giant Jenga Game",
+      "Item Description": "Giant Jenga game",
+      "Quantity Available": 1,
+      "Rental Price": 25,
+      "Category": "Games",
+      "Image": "https://i5.walmartimages.com/seo/Trimate-Tumbling-Tower-Large-21-25-Double-Sanded-Smooth-Natural-Wood-54-Blocks-Durable-Portable-Carry-Bag-Indoor-Outdoor-Family-Party-Game-Age-8_9274de07-c8fd-4d0b-8249-1e555fbbdce5.54354e63fb4e82599fefa86bdcea4a1f.jpeg?odnHeight=573&odnWidth=573&odnBg=FFFFFF"
+    },
+    {
+      "Item_ID": "Popcorn_Machine",
+      "Product_ID": "7069036000001340001",
+      "Item Name": "Popcorn Machine",
+      "Item Description": "Standing Popcorn Machine",
+      "Quantity Available": 1,
+      "Rental Price": 75,
+      "Category": "Other",
+      "Image": "https://assets.wfcdn.com/im/69633876/resize-h600-w600%5Ecompr-r85/2346/234694232/Nostalgia+Vintage+Professional+Popcorn+Cart+-+8-Ounce+Kettle+-+53+Inches+Tall.jpg"
+    },
+    {
+      "Item_ID": "Pink_Table_Runner",
+      "Product_ID": "7069036000001467020",
+      "Item Name": "Pink Table Runner",
+      "Item Description": "Light pink table runner",
+      "Quantity Available": 4,
+      "Rental Price": 2,
+      "Category": "Table Runners",
+      "Image": "https://redrockpartyrentals.com/images/tablerunner-lightpink.jpg"
+    },
+    {
+      "Item_ID": "Beige_Table_Runner",
+      "Product_ID": "7069036000001467025",
+      "Item Name": "Beige Table Runner",
+      "Item Description": "Beige table runner",
+      "Quantity Available": 4,
+      "Rental Price": 2,
+      "Category": "Table Runners",
+      "Image": "https://redrockpartyrentals.com/images/tablerunner-beige.jpg"
+    },
+    {
+      "Item_ID": "Orange_Table_Runner",
+      "Product_ID": "7069036000001467030",
+      "Item Name": "Orange Table Runner",
+      "Item Description": "Orange table runner",
+      "Quantity Available": 4,
+      "Rental Price": 2,
+      "Category": "Table Runners",
+      "Image": "https://redrockpartyrentals.com/images/tablerunner-dustyorange.jpg"
+    }
+
+];
