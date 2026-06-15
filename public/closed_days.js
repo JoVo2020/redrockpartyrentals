739027835de8cb547d712c1b969ef2ad66f3a5cb
JoVo2020 <joel.m.voss@gmail.com>
2026-06-15 07:11:55 -0700

    Update closed_days.js

--- public/closed_days.js
+++ public/closed_days.js
@@ -15,6 +15,36 @@ window.CLOSED_DAYS_OVERRIDES = [
     ]
   },
   {
+    label: "July 3rd 2026",
+    event_date_start: "2026-07-03",
+    event_date_end:   "2026-07-03",
+    dropoff: [
+      { date: "2026-07-01", time: "5:00 PM – 8:00 PM", default: false },
+      { date: "2026-07-02", time: "5:00 PM – 8:00 PM", default: true },
+      { date: "2026-07-03", time: "5:00 PM – 8:00 PM", default: false  }
+    ],
+    pickup: [
+      { date: "2026-07-04",  time: "8:00 AM – 11:00 AM", default: true  },
+      { date: "2026-07-05", time: "8:00 AM – 11:00 AM", default: false },
+      { date: "2026-07-05", time: "5:00 PM – 8:00 PM", default: false }
+    ]
+  },
+  {
+    label: "July 4th-5th 2026",
+    event_date_start: "2026-07-04",
+    event_date_end:   "2026-07-05",
+    dropoff: [
+      { date: "2026-07-02", time: "5:00 PM – 8:00 PM", default: false },
+      { date: "2026-07-03", time: "5:00 PM – 8:00 PM", default: true },
+      { date: "2026-07-04",  time: "8:00 AM – 11:00 AM", default: false  }
+    ],
+    pickup: [
+      { date: "2026-07-05", time: "8:00 AM – 11:00 AM", default: false  },
+      { date: "2026-07-05", time: "5:00 PM – 8:00 PM", default: false },
+      { date: "2026-07-06", time: "5:00 PM – 8:00 PM", default: true }
+    ]
+  },
+  {
     label: "Seattle Trip 2026",
     event_date_start: "2026-07-09",
     event_date_end:   "2026-07-15",
